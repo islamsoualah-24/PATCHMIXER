50f9e8ff483fc4cd72c0343fb3122c6a3bc5154a
islamsoualah-24 <islamsoualah2017@gmail.com>
2026-08-04 11:05:19 +0100

    Update tsmixer.py

--- models/tsmixer.py
+++ models/tsmixer.py
@@ -1,187 +1,304 @@
 # coding=utf-8
-"""
-PyTorch implementation of TSMixer
-Compatible with PatchMixer / PyTorch training pipeline.
-"""
 
 import torch
 import torch.nn as nn
+import torch.fft
 
 
-class ResBlock(nn.Module):
-    """
-    TSMixer residual block.
+class RevIN(nn.Module):
+    def __init__(self, num_features: int, eps=1e-5, affine=True, subtract_last=False):
+        super(RevIN, self).__init__()
 
-    Input:
-        [B, L, C]
+        self.num_features = num_features
+        self.eps = eps
+        self.affine = affine
+        self.subtract_last = subtract_last
 
-    Output:
-        [B, L, C]
-    """
+        self.mean = None
+        self.stdev = None
+        self.last = None
 
-    def __init__(
-        self,
-        seq_len,
-        n_channels,
-        norm_type='L',
-        activation='relu',
-        dropout=0.2,
-        ff_dim=2048
-    ):
-        super().__init__()
+        if self.affine:
+            self._init_params()
 
-        # -------------------------------------------------
-        # Normalization
-        # -------------------------------------------------
-        if norm_type == 'L':
-            self.norm1 = nn.LayerNorm(n_channels)
-            self.norm2 = nn.LayerNorm(n_channels)
+    def forward(self, x, mode: str):
+
+        if mode == 'norm':
+            self._get_statistics(x)
+            x = self._normalize(x)
+
+        elif mode == 'denorm':
+            x = self._denormalize(x)
+
         else:
-            self.norm1 = nn.BatchNorm1d(n_channels)
-            self.norm2 = nn.BatchNorm1d(n_channels)
+            raise NotImplementedError
 
-        # -------------------------------------------------
-        # Activation
-        # -------------------------------------------------
-        if activation.lower() == 'relu':
-            act = nn.ReLU()
-        elif activation.lower() == 'gelu':
-            act = nn.GELU()
-        elif activation.lower() == 'silu':
-            act = nn.SiLU()
-        else:
-            act = nn.ReLU()
+        return x
 
-        # -------------------------------------------------
-        # Temporal Linear
-        #
-        # [B, L, C]
-        # -> [B, C, L]
-        # -> Linear(L, L)
-        # -> [B, L, C]
-        # -------------------------------------------------
-        self.temporal_mlp = nn.Sequential(
-            nn.Linear(seq_len, seq_len),
-            act,
-            nn.Dropout(dropout)
+    def _init_params(self):
+        self.affine_weight = nn.Parameter(
+            torch.ones(self.num_features)
+        )
+        self.affine_bias = nn.Parameter(
+            torch.zeros(self.num_features)
         )
 
-        # -------------------------------------------------
-        # Feature Linear
-        # -------------------------------------------------
-        self.feature_mlp = nn.Sequential(
-            nn.Linear(n_channels, ff_dim),
-            act,
-            nn.Dropout(dropout),
-            nn.Linear(ff_dim, n_channels),
-            nn.Dropout(dropout)
-        )
+    def _get_statistics(self, x):
 
-    def forward(self, x):
+        dim2reduce = tuple(range(1, x.ndim - 1))
 
-        # =================================================
-        # Temporal Mixing
-        # =================================================
+        if self.subtract_last:
+            self.last = x[:, -1, :].unsqueeze(1)
+        else:
+            self.mean = torch.mean(
+                x,
+                dim=dim2reduce,
+                keepdim=True
+            ).detach()
 
-        residual = x
+        self.stdev = torch.sqrt(
+            torch.var(
+                x,
+                dim=dim2reduce,
+                keepdim=True,
+                unbiased=False
+            ) + self.eps
+        ).detach()
 
-        x = self.norm1(x)
+    def _normalize(self, x):
 
-        # [B, L, C] -> [B, C, L]
-        x = x.transpose(1, 2)
+        if self.subtract_last:
+            x = x - self.last
+        else:
+            x = x - self.mean
 
-        # Temporal MLP
-        x = self.temporal_mlp(x)
+        x = x / self.stdev
 
-        # [B, C, L] -> [B, L, C]
-        x = x.transpose(1, 2)
+        if self.affine:
+            x = x * self.affine_weight
+            x = x + self.affine_bias
 
-        x = x + residual
+        return x
 
-        # =================================================
-        # Feature Mixing
-        # =================================================
+    def _denormalize(self, x):
 
-        residual = x
+        if self.affine:
+            x = x - self.affine_bias
+            x = x / (self.affine_weight + self.eps * self.eps)
 
-        x = self.norm2(x)
+        x = x * self.stdev
 
-        x = self.feature_mlp(x)
-
-        x = x + residual
+        if self.subtract_last:
+            x = x + self.last
+        else:
+            x = x + self.mean
 
         return x
 
 
-class Model(nn.Module):
-    """
-    TSMixer model compatible with PatchMixer Exp_Main.
+class Mlp_feat(nn.Module):
 
-    Input:
-        [B, seq_len, enc_in]
+    def __init__(
+        self,
+        in_features,
+        hidden_features=None,
+        out_features=None,
+        drop=0.
+    ):
+        super(Mlp_feat, self).__init__()
 
-    Output:
-        [B, pred_len, enc_in]
-    """
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
 
-    def __init__(self, args):
-        super(Model, self).__init__()
+        self.fc1 = nn.Linear(
+            in_features,
+            hidden_features
+        )
 
-        self.seq_len = args.seq_len
-        self.pred_len = args.pred_len
-        self.enc_in = args.enc_in
+        self.act = nn.ReLU()
 
-        self.norm_type = args.norm_type
-        self.activation = args.activation
-        self.n_block = args.n_block
-        self.dropout = args.dropout
-        self.ff_dim = args.ff_dim
+        self.fc2 = nn.Linear(
+            hidden_features,
+            out_features
+        )
 
-        # -------------------------------------------------
-        # TSMixer blocks
-        # -------------------------------------------------
+        self.drop = nn.Dropout(drop)
 
-        self.blocks = nn.ModuleList([
-            ResBlock(
-                seq_len=self.seq_len,
-                n_channels=self.enc_in,
-                norm_type=self.norm_type,
-                activation=self.activation,
-                dropout=self.dropout,
-                ff_dim=self.ff_dim
-            )
-            for _ in range(self.n_block)
-        ])
+    def forward(self, x):
 
-        # -------------------------------------------------
-        # Forecasting head
-        #
-        # [B, L, C]
-        # -> [B, C, L]
-        # -> Dense(L -> pred_len)
-        # -> [B, pred_len, C]
-        # -------------------------------------------------
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
 
-        self.forecast = nn.Linear(
+        x = self.fc2(x)
+        x = self.drop(x)
+
+        return x
+
+
+class Mlp_time(nn.Module):
+
+    def __init__(
+        self,
+        in_features,
+        hidden_features=None,
+        out_features=None,
+        drop=0.
+    ):
+        super(Mlp_time, self).__init__()
+
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+
+        self.fc1 = nn.Linear(
+            in_features,
+            hidden_features
+        )
+
+        self.act = nn.ReLU()
+
+        self.drop = nn.Dropout(drop)
+
+    def forward(self, x):
+
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.drop(x)
+
+        return x
+
+
+class Mixer_Layer(nn.Module):
+
+    def __init__(self, time_dim, feat_dim):
+
+        super(Mixer_Layer, self).__init__()
+
+        self.batchNorm2D = nn.BatchNorm1d(time_dim)
+
+        self.MLP_time = Mlp_time(
+            time_dim,
+            time_dim
+        )
+
+        self.MLP_feat = Mlp_feat(
+            feat_dim,
+            feat_dim
+        )
+
+    def forward(self, x):
+
+        # [B, L, D]
+        res1 = x
+
+        x = self.batchNorm2D(x)
+
+        # Temporal mixing
+        x = self.MLP_time(
+            x.permute(0, 2, 1)
+        ).permute(0, 2, 1)
+
+        x = x + res1
+
+        # Feature mixing
+        res2 = x
+
+        x = self.batchNorm2D(x)
+
+        x = self.MLP_feat(x)
+
+        x = x + res2
+
+        return x
+
+
+class Backbone(nn.Module):
+
+    def __init__(self, configs):
+
+        super(Backbone, self).__init__()
+
+        self.seq_len = configs.seq_len
+        self.pred_len = configs.pred_len
+        self.enc_in = configs.enc_in
+
+        self.layer_num = 1
+
+        self.mix_layer = Mixer_Layer(
+            self.seq_len,
+            self.enc_in
+        )
+
+        self.temp_proj = nn.Linear(
             self.seq_len,
             self.pred_len
         )
 
     def forward(self, x):
 
-        # x:
-        # [B, seq_len, enc_in]
+        # [B, L, D]
+        x = self.mix_layer(x)
 
-        for block in self.blocks:
-            x = block(x)
+        # [B, L, D]
+        # ->
+        # [B, D, L]
+        x = x.permute(0, 2, 1)
 
-        # [B, L, C] -> [B, C, L]
-        x = x.transpose(1, 2)
+        # [B, D, L]
+        # ->
+        # [B, D, H]
+        x = self.temp_proj(x)
 
-        # [B, C, L] -> [B, C, pred_len]
-        x = self.forecast(x)
-
-        # [B, C, pred_len] -> [B, pred_len, C]
-        x = x.transpose(1, 2)
+        # [B, D, H]
+        # ->
+        # [B, H, D]
+        x = x.permute(0, 2, 1)
 
         return x
+
+
+class TSMixer(nn.Module):
+
+    def __init__(self, configs):
+
+        super(TSMixer, self).__init__()
+
+        self.rev = RevIN(
+            configs.enc_in
+        )
+
+        self.backbone = Backbone(
+            configs
+        )
+
+        self.seq_len = configs.seq_len
+        self.pred_len = configs.pred_len
+
+    def forward(self, x):
+
+        # Normalize
+        z = self.rev(
+            x,
+            'norm'
+        )
+
+        # TSMixer
+        z = self.backbone(z)
+
+        # De-normalize
+        z = self.rev(
+            z,
+            'denorm'
+        )
+
+        return z
+
+
+# ============================================================
+# PATCHMIXER COMPATIBILITY WRAPPER
+# ============================================================
+
+class Model(TSMixer):
+
+    def __init__(self, configs):
+        super(Model, self).__init__(configs)
